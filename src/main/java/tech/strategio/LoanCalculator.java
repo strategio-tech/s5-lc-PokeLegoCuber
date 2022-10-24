ad0fa01aa949bf8b60aa19710d93cbd089b5fb46
pokelegocuber <pokelegocuber@yahoo.com>
2022-10-24 12:42:15 -0400

    Finished

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,16 +7,29 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * This method takes in a loan amount and calculates the payment for the next 3 months.
+     * It then outputs the month, the payment due, and the remaining amount of the loan you have.
+     *
+     * @param amount this is this loan amount
+     * @return the remaining amount after 3 months
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Add your code below
+        for (int i = 0; i < 3; i++)
+        {
+            int old_amount = amount;
+            int ten_percent_amount = (int)(amount * 0.10);
+            amount = amount - ten_percent_amount;
+            System.out.println("Month " + (i+1));
+            System.out.println("Payment: 10% of "+ old_amount + " = " + ten_percent_amount);
+            System.out.println("Remaining amount: " + amount);
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number:");
         int amount = scanner.nextInt();
         System.out.println(getRemainingAmountIn3Months(amount));
     }
